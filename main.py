00f6957acf4080031d4d044cae8f0f7713333375
abellaouikenza <127732265+abellaouikenza@users.noreply.github.com>
2024-04-22 18:47:08 +0200

    data ingestion, validation and transformation notebooks added

---
+++ main.py
@@ -0,0 +1,36 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.logging import logger
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>>> {STAGE_NAME} started <<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>> {STAGE_NAME} completed <<<<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>>>>  {STAGE_NAME} started <<<<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>>>>>  {STAGE_NAME} completed <<<<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>>>>  {STAGE_NAME} started <<<<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>>>>>  {STAGE_NAME} completed <<<<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
